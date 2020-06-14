2a235e1397f881aec65666d79a2631ab22788af7
Gintare <statkute.g@gmail.com>
2020-06-14 12:39:04 +0100

    created an xcode project

---
+++ frontend/parceltracker/parceltracker/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  parceltracker
+//
+//  Created by Gintare Statkute on 2020-06-14.
+//  Copyright © 2020 Bourn32. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "archivebox")
+                        Text("Parcels")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "gear")
+                        Text("Settings")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
