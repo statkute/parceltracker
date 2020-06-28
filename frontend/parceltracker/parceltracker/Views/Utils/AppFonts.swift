c6480f8af44a5b405fb2d0315eb37b6b7ecea135
Hlev1 <harrylevick@hotmail.co.uk>
2020-06-28 21:32:36 +0100

    Create AppFonts.swift

---
+++ frontend/parceltracker/parceltracker/Views/Utils/AppFonts.swift
@@ -0,0 +1,16 @@
+//
+//  AppFonts.swift
+//  Parceltracker
+//
+//  Created by Harry Levick on 28/06/2020.
+//
+import SwiftUI
+
+let HeadingSize = CGFloat(25)
+let SubHeadingSize = CGFloat(14)
+// IconSize and TrackBarWidth both have the same font weight
+let IconSize = CGFloat(22)
+let TrackBarWidth = CGFloat(2.3)
+
+let DeliveredIcon = "cube.box"
+let PendingDeliveredIcon = "arrow.up.circle"
