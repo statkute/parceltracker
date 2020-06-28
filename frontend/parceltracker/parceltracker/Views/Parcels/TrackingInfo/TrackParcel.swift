0aa6847b1eb4d8b5035002bbdacd93fda5e468e0
Hlev1 <harrylevick@hotmail.co.uk>
2020-06-28 18:35:50 +0100

    add TrackingInfo attribute

--- frontend/parceltracker/parceltracker/Views/Parcels/TrackingInfo/TrackParcel.swift
+++ frontend/parceltracker/parceltracker/Views/Parcels/TrackingInfo/TrackParcel.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct TrackParcel: View {
     var parcelName : String
+    var trackingInfo : TrackingInfo
 
-    init(name : String) {
+    init(tracking : TrackingInfo, name : String) {
+        trackingInfo = tracking
         parcelName = name
         UITableView.appearance().separatorColor = .clear
     }
@@ -20,6 +22,8 @@ struct TrackParcel: View {
                 
             ScrollView() {
                 GeometryReader { (stackProxy: GeometryProxy) in
+                    Text(self.trackingInfo.courier.name)
+                    Text(self.trackingInfo.trackingNumber)
                     VStack (spacing: 0) {
                         
                         GeometryReader { (symbolProxy: GeometryProxy) in
@@ -28,14 +32,18 @@ struct TrackParcel: View {
                                     .frame(height: 23)
                                     .offset(x: symbolProxy.size.width/60)
                                     .font(Font.system(size: 22,weight: .semibold))
+                                Image(systemName: self.trackingInfo.delivered ? "cube.box" : "arrow.up.circle")
+                                    .foregroundColor(self.trackingInfo.delivered ? .green : .black)
                             }
                         }
                         Spacer()
                         ForEach(parcelLifespan) { update in
+                        ForEach(self.trackingInfo.trackingInfo) { update in
                             ParcelStatus(status: update)
                                 //.listRowInsets(EdgeInsets())
                                 .padding(.leading)
                                 .border(width: 2.3, edge: .leading, color: .black)
+                                        color: (self.trackingInfo.delivered ? .green : .black))
                         }
                     }
                 }
@@ -51,5 +59,6 @@ struct TrackParcel: View {
 struct TrackParcel_Previews: PreviewProvider {
     static var previews: some View {
         TrackParcel(name: "Books from Ebay")
+        TrackParcel(tracking: trackingInfo, name: "Books from Ebay")
     }
 }
