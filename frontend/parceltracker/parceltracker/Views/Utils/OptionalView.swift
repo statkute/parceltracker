9a034c13e7c5f4da7894398dfb36a0e54cdb5d96
Hlev1 <harrylevick@hotmail.co.uk>
2020-06-28 19:54:52 +0100

    Create OptionalView.swift

---
+++ frontend/parceltracker/parceltracker/Views/Utils/OptionalView.swift
@@ -0,0 +1,48 @@
+//
+//  OptionalView.swift
+//  Parceltracker
+//
+//  Created by Harry Levick on 28/06/2020.
+//
+import SwiftUI
+
+struct OptionalView<Value, Content>: View where Content: View {
+    var content: (Value) -> Content
+    var value: Value
+    
+    init?(_ value: Value?, @ViewBuilder content: @escaping (Value) -> Content) {
+        guard let value = value else {
+            return nil
+        }
+        self.value = value
+        self.content = content
+    }
+    
+    var body: some View {
+        content(value)
+    }
+}
+
+extension Optional where Wrapped: View {
+    func fallbackView<T: View>(_ transform: () -> T) -> AnyView? {
+        switch self {
+            case .none:
+                return AnyView(transform())
+            case .some(let view):
+                return AnyView(view)
+        }
+    }
+    
+    func fallbackView<T: View, Value>(_ value: Value?, _ transform: (Value) -> T) -> AnyView? {
+        switch self {
+            case .none:
+                if let unwrapped = value {
+                    return AnyView(transform(unwrapped))
+                } else {
+                    return nil
+            }
+            case .some(let view):
+                return AnyView(view)
+        }
+    }
+}
