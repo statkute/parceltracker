a9f4f4f2d30cee6279ed3b561fbc9398d45fad46
Hlev1 <harrylevick@hotmail.co.uk>
2020-06-29 20:11:25 +0100

    remove unused HStack

--- frontend/parceltracker/parceltracker/Views/Parcels/TrackingInfo/ParcelStatusList.swift
+++ frontend/parceltracker/parceltracker/Views/Parcels/TrackingInfo/ParcelStatusList.swift
@@ -16,17 +16,14 @@ struct ParcelStatusList: View {
             VStack (spacing: 0) { // spacing: 0 ensures no spacing between vertical items
                 
                 GeometryReader { (symbolProxy: GeometryProxy) in
-                    HStack {
-                        
-                        Image(systemName: self.delivered ? DeliveredIcon : PendingDeliveredIcon)
-                            .padding(.top, 5) // padding to ensure icon fits into view
-                            // use width of scroll bar to place icon at the end of the left border
-                            // found basically using trial and error and it works ¯\_(ツ)_/¯
-                            // But since it uses the width of the parent view, this is pretty robust for all devices.
-                            .offset(x: symbolProxy.size.width/59.5)
-                            .font(Font.system(size: IconSize, weight: .semibold))
-                            .foregroundColor(self.delivered ? .green : .black)
-                    }
+                    Image(systemName: self.delivered ? DeliveredIcon : PendingDeliveredIcon)
+                        .padding(.top, 5) // padding to ensure icon fits into view
+                        // use width of scroll bar to place icon at the end of the left border
+                        // found basically using trial and error and it works ¯\_(ツ)_/¯
+                        // But since it uses the width of the parent view, this is pretty robust for all devices.
+                        .offset(x: symbolProxy.size.width/59.5)
+                        .font(Font.system(size: IconSize, weight: .semibold))
+                        .foregroundColor(self.delivered ? .green : .black)
                 }
                 
                 Spacer()
