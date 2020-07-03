9fa2c8866f5df303ea17fd9e2b7a4abc8bdd5d37
Gintare <statkute.g@gmail.com>
2020-07-03 19:42:11 +0100

    parcelFilter color improvements

--- frontend/parceltracker/parceltracker/ParcelsHomeTab.swift
+++ frontend/parceltracker/parceltracker/ParcelsHomeTab.swift
@@ -17,7 +17,7 @@ struct ParcelsHomeTab: View {
                 .navigationBarTitle(Text("Your Parcels"))
                 .navigationBarItems(trailing:
                     HStack {
-                        ParcelListFilter(selectedParcelFilter: self.selectedParcelFilter).padding(.trailing, (geometry.size.width / 2.0) - (102.5 + 47.5))
+                        ParcelListFilter(selectedParcelFilter: self.selectedParcelFilter).padding(.trailing, (geometry.size.width / 2.0) - (102.5 + 45))
                         Spacer()
                         Button(action: {}) {
                             Image(systemName: "plus").foregroundColor(.accentColor).imageScale(.large)
@@ -32,9 +32,11 @@ struct ParcelsHomeTab: View {
 struct ParcelsHomeTab_Previews: PreviewProvider {
     static var previews: some View {
         ParcelsHomeTab()
+            .environment(\.colorScheme, .light)
     }
 }
 
 class SelectedParcelFilter: ObservableObject{
-    @Published var filter: ParcelFilter = ParcelFilter.Upcoming;
+    @Published var filter: ParcelFilter = ParcelFilter.Past
+    ;
 }
--- frontend/parceltracker/parceltracker/Views/Utils/ParcelListFilter.swift
+++ frontend/parceltracker/parceltracker/Views/Utils/ParcelListFilter.swift
@@ -17,7 +17,7 @@ struct ParcelListFilter: View {
             Capsule()
                 .frame(width: 210, height: 35)
                 .cornerRadius(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-                .foregroundColor(Color(colorScheme == .dark ? UIColor.secondarySystemBackground : UIColor.secondaryLabel)).opacity(0.4)
+                .foregroundColor(Color(colorScheme == .dark ? UIColor.secondarySystemBackground : UIColor.secondaryLabel)).opacity(colorScheme == .dark ? 0.9 : 0.3)
             
             HStack {
                 FilterScope(selectedParcelFilter: selectedParcelFilter, filterName: "Upcoming", activeOnUpcomingFilterValue: ParcelFilter.Upcoming)
@@ -37,13 +37,14 @@ struct ScopeFilter_Previews: PreviewProvider {
 
 struct FilterScope: View {
     @ObservedObject var selectedParcelFilter: SelectedParcelFilter
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
     var filterName: String
     var activeOnUpcomingFilterValue: ParcelFilter
     var body: some View {
         ZStack {
             Capsule()
                 .frame(width: 100, height: 30)
-                .foregroundColor(Color(UIColor.white).opacity(self.activeOnUpcomingFilterValue == self.selectedParcelFilter.filter ? 0.45 : 0))
+                .foregroundColor(Color(UIColor.white).opacity(self.activeOnUpcomingFilterValue == self.selectedParcelFilter.filter ? (colorScheme == .dark ? 0.2 : 0.8) : 0))
                 .cornerRadius(1.0)
             
             Text(self.filterName)
